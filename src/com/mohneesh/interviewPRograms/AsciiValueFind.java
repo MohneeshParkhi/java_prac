f48b61d55b8b3172b569371796cc4f77297c9446
mohneeshParkhi <mohneesh0088@gmail.com>
2019-06-27 17:21:08 +0530

    further practice of anagram

--- src/com/mohneesh/interviewPRograms/AsciiValueFind.java
+++ src/com/mohneesh/interviewPRograms/AsciiValueFind.java
@@ -14,17 +14,36 @@ public static void main(String[] args) {
 	
 	String str = scan.nextLine();
 	
-	char []checkPangram = new char[26];
+	boolean []checkPangram = new boolean[26];
 	
 	char [] arr = str.toLowerCase().toCharArray();
 	
 	for(int i=0; i < arr.length;i++) {
 		char c = arr[i];
 		Integer iNum =(int) c;
-		System.out.println(iNum);
+		System.out.println(iNum-96);
+		
+	}
+	
+	for(int i=97 ;i<122 ;i++) {
+		for(int j=0 ;j<arr.length;j++) {
+			char c = arr[i];
+			Integer iNum =(int)c;
+			int kPos = iNum-96;
+			
+			if(iNum == i) {
+				
+				checkPangram[kPos]=true;
+			}else {
+				checkPangram[kPos] = false;
+			}
+		}
 	}
 	
 	
+	for(int h=0 ;h < 25 ;h++) {
+	System.out.println(checkPangram[h]);
+	}
 	
 	
 }
